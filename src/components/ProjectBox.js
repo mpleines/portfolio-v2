cc98450b49b6705311337190e8f2a8b8a55f5eca
devilsdev <maik.pleines@googlemail.com>
2019-12-01 13:33:53 +0100

    changed styles of projects

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -12,8 +12,8 @@ const ProjectBox = props => {
                 <p>{description}</p>
             </div>
             <div className="project-links">
-                <a href={links.demo} className="small-link">Show Project</a>
-                <a href={links.source} className="small-link">Show Source Code</a>
+                <a href={links.demo} className="small-link small-link-green">Show Project</a>
+                <a href={links.source} className="small-link small-link-grey">Show Source Code</a>
             </div>
         </div>
     )
