19476e61492e22035cc4c49535143b1dc91d010d
devilsdev <maik.pleines@googlemail.com>
2019-12-29 11:34:35 +0100

    changed spacing in footer

--- src/Contact.js
+++ src/Contact.js
@@ -3,11 +3,11 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <form className="contact">
+    <form className="contact" action="mailto:maik.pleines@googlemail.com" method="post">
         <h2 id="contact" className="contactTitle">SAY HELLO</h2>
-        <input type="text" placeholder="Your name" className="contactInput"/>
-        <input type="email" placeholder="Your email" className="contactInput"/>
-        <textarea placeholder="Your message" className="contactInput"></textarea>
+        <input type="text" placeholder="Your name" className="contactInput" name="name"/>
+        <input type="email" placeholder="Your email" className="contactInput" name="mail"/>
+        <textarea placeholder="Your message" className="contactInput" name="comment"></textarea>
         <button type="submit" className="sendButton">SEND</button>
     </form>
   );
