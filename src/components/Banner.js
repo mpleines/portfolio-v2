a9faf49a6f36005c6f4d70f5696d9cb39e75b003
devilsdev <maik.pleines@googlemail.com>
2019-12-29 15:27:58 +0100

    jsx to css on banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,47 +1,18 @@
 import React from 'react';
+import './Banner.css';
+
 import attention from '../assets/attention.svg';
 
 const Banner = () => {
-
-  const styles = {
-    wrapper: {
-        margin: '0 auto',
-        marginTop: '1em',
-        marginBottom: '1em',
-        maxWidth: '550px',
-        padding: '10px',
-        borderRadius: '9px',
-        backgroundColor: '#8333FF',
-        display: 'flex',
-        justifyContent: 'center',
-    },
-    paragraph: {
-        textAlign: 'left'
-    },
-    link: {
-        color: 'white',
-        '&:hover': {
-            textDecoration: 'underline'
-        },
-        '&:visited': {
-            color: 'white'
-        }
-    },
-    attention: {
-        width: '2em',
-        height: 'auto',
-        marginRight: '15px',
-        filter: 'invert(1)'
-    }
-  };
-
   return (
-    <div style={styles.wrapper}>
-        <img src={attention} alt="exclamation mark" style={styles.attention}/>
-        <p style={styles.paragraph}>
-            Currently I am working on <a href="https://wizardly-bose-04d5eb.netlify.com/" style={styles.link}>Clonergram</a> and <a href="https://maikpleines.com" style={styles.link}>this website</a>
+    <blockquote className="banner-wrapper">
+        <img src={attention} alt="exclamation mark" className="banner-icon"/>
+        <p className="banner-paragraph">
+            Currently I am working on 
+            <a href="https://wizardly-bose-04d5eb.netlify.com/" className="banner-link">Clonergram</a>
+            and <a href="https://maikpleines.com" className="banner-link">this website</a>
         </p>
-    </div>
+    </blockquote>
   );
 }
 
