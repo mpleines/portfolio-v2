b1ec2dc36e4ba9b12e2792c5f646a342be3b5e51
devilsdev <maik.pleines@googlemail.com>
2019-12-29 15:57:14 +0100

    changed width etc. of banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
   return (
     <blockquote className="banner-wrapper">
         <img src={attention} alt="exclamation mark" className="banner-icon"/>
-        <p>
+        <p className="banner-paragraph">
             Currently I am working on 
             <a href="https://wizardly-bose-04d5eb.netlify.com/" className="banner-link">Clonergram</a>
             and <a href="https://maikpleines.com" className="banner-link">this website</a>
