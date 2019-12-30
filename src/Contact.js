b71123a2c8a8b39a6a74b9ca4b10970eb1b0824b
devilsdev <maik.pleines@googlemail.com>
2019-12-30 16:20:56 +0100

    contact form validation

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Contact.css';
 import sendMail from './mailsender';
+import { toast } from 'react-toastify';
 
 const Contact = () => {
   const [name, setName] = useState('');
@@ -9,6 +10,22 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if(!validateInput(name)) {
+      showErrorToast('Please enter a valid name');
+      return false;
+    }
+
+    if(!validateEmail(emailFrom)) {
+      showErrorToast('Please enter a valid email address');
+      return false;
+    }
+
+    if(!validateInput(text)) {
+      showErrorToast('Please enter a valid message');
+      return false;
+    }
+
     sendMail({
       name: name,
       emailFrom: emailFrom,
@@ -16,13 +33,36 @@ const Contact = () => {
     });
   }
 
+  const validateInput = (text) => {
+    return text.length > 1;
+  }
+
+  const validateEmail = (email) => {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+  }
+
+  const showErrorToast = (message) => {
+    toast.error(message, {
+      position: toast.POSITION.BOTTOM_CENTER
+    })
+  }
+
   return (
     <form id="contact-form" className="contact">
         <h2 id="contact" className="contactTitle">SAY HELLO</h2>
-        <input type="text" placeholder="Your name" className="contactInput" name="name" onChange={e => setName(e.target.value)}/>
-        <input type="email" placeholder="Your email" className="contactInput" name="mail" onChange={e => setEmailFrom(e.target.value)}/>
-        <textarea placeholder="Your message" className="contactInput" name="comment" onChange={e => setText(e.target.value)}></textarea>
-        <button type="submit" className="sendButton" onClick={e => handleSubmit(e)}>SEND</button>
+        <input type="text" placeholder="Your name" className="contactInput" name="name" 
+          onChange={e => setName(e.target.value)}
+        />
+        <input type="email" placeholder="Your email" className="contactInput" name="mail" 
+          onChange={e => setEmailFrom(e.target.value)}
+        />
+        <textarea placeholder="Your message" className="contactInput" name="comment" 
+          onChange={e => setText(e.target.value)}
+        ></textarea>
+        <button type="submit" 
+          className="sendButton"
+          onClick={e => handleSubmit(e)}
+        >SEND</button>
     </form>
   );
 }
