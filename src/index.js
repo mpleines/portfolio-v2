eff54858c7b7878884c34ddadf9e92668f6c910c
devilsdev <maik.pleines@googlemail.com>
2020-01-05 18:22:04 +0100

    ..

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 // Google Analytics
 import ReactGA from 'react-ga';
-const trackingId = process.env.googleAnalyticsId; 
+const trackingId = 'UA-146888592-1'; 
 ReactGA.initialize(trackingId);
 
 ReactDOM.render(<App />, document.getElementById('root'));
