eb0d19429c32a0b5811638675851a30d6b79c647
devilsdev <maik.pleines@googlemail.com>
2020-01-05 18:25:41 +0100

    ..

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import App from './App';
 import ReactGA from 'react-ga';
 const trackingId = 'UA-146888592-1'; 
 ReactGA.initialize(trackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
