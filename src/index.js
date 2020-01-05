bfcdd91a43081d051864caf53456c2c6e1a11e97
devilsdev <maik.pleines@googlemail.com>
2020-01-05 18:18:07 +0100

    added google analytics

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Google Analytics
+import ReactGA from 'react-ga';
+const trackingId = process.env.googleAnalyticsId; 
+ReactGA.initialize(trackingId);
+
 ReactDOM.render(<App />, document.getElementById('root'));
